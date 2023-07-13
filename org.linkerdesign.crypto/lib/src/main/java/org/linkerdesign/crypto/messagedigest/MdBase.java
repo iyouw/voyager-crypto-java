70fac2be9b01a13e7224fadc244a573b4b7c75d7
iwander <231779192@qq.com>
2023-07-13 16:23:05 +0800

    chore(crypto): remove unused code

--- org.linkerdesign.crypto/lib/src/main/java/org/linkerdesign/crypto/messagedigest/MdBase.java
+++ org.linkerdesign.crypto/lib/src/main/java/org/linkerdesign/crypto/messagedigest/MdBase.java
@@ -1,7 +1,6 @@
 package org.linkerdesign.crypto.messagedigest;
 
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 
 import org.linkerdesign.crypto.CryptoBase;
 import org.linkerdesign.crypto.ReadCallback;
